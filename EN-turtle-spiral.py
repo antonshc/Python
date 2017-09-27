5344081aeef2bf2eeeab190ad2b1f42daa57dcb5
Mustafa Çalap <3nexus8@gmail.com>
2017-09-27 15:01:47 +0300

    Turtle draws a spiral

--- EN-turtle-spiral.py
+++ EN-turtle-spiral.py
@@ -8,7 +8,7 @@ tess.color("blue")
 tess.shape("turtle")
 
 print(list(range(5, 90, 2)))
-tess.up()                     # this is new
+tess.up()                       # this is new
 for size in range(5, 90, 2):    # start with size = 5 and grow by 2
     tess.stamp()                # leave an impression on the canvas
     tess.forward(size)          # move tess along
