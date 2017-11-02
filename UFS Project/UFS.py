d53e4af5f27ef6e9db194a46421ee567fca61679
Mustafa Çalap <3nexus8@gmail.com>
2017-11-02 17:04:11 +0300

    added 7zip app

--- UFS Project/UFS.py
+++ UFS Project/UFS.py
@@ -97,6 +97,8 @@ def ana_döngü():
                 yükle_temapaketi()
             elif a == "23":
                 yükle_trimage()
+            elif a == "24":
+                yükle_7zip()
             elif a == "999":
                 sistem_güncelle(güncellendikten_sonra_sistemi_kapat='hayır')
                 yükle_filezilla()
@@ -122,6 +124,7 @@ def ana_döngü():
                 yükle_guvcview()
                 yükle_temapaketi()
                 yükle_trimage()
+                yükle_7zip()
             elif a == "0":
                 os.system('clear')
             elif a == "h":
@@ -313,7 +316,7 @@ komutlar = """\
 {4}[{0}14{4}] {1}Skype                       {4}|   [{0}20{4}]  {1}Steam platformu
 {4}-----------------------------------------------------------------------------------{4}
 {4}[{0}21{4}] {1}Guvcview                    {4}|   [{0}22{4}]  {1}Tema Paketi
-{4}[{0}23{4}] {1}Trimage
+{4}[{0}23{4}] {1}Trimage                     {4}|   [{0}24{4}]  {1}7zip FULL+RAR
 
 """  # Buraya yeni özellikleri eklemeyi unutma, eklemezsen ekranda görünmez
 def geçici_dizin_oluştur():
@@ -600,6 +603,16 @@ def yükle_trimage():
     except:
         os.system('clear')
         print('\nTrimage uygulaması başarıyla KURULAMADI')
+def yükle_7zip():
+    try:
+        os.system('clear')
+        print('\n7zip uygulaması kuruluyor\n')
+        os.system('sudo apt-get install p7zip --yes && sudo apt-get install p7zip-full --yes && sudo apt-get install p7zip-rar --yes')
+        os.system('clear')
+        print('\n7zip uygulaması başarıyla kuruldu\n')
+    except:
+        os.system('clear')
+        print('\n7zip uygulaması başarıyla KURULAMADI')
 
 def sistem_güncelle(güncellendikten_sonra_sistemi_kapat):
     if güncellendikten_sonra_sistemi_kapat == 'evet':
